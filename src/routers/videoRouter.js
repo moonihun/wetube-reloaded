4fc8411fa45c94557785f24392b8999ae56ee4ed
moonihun <anslgns1@gmail.com>
2021-11-09 17:36:00 +0900

    heroku deploy

---
+++ src/routers/videoRouter.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import {
+  watch,
+  postEdit,
+  getEdit,
+  getUpload,
+  postUpload,
+  deleteVideo,
+} from '../controllers/videoController';
+import { protectorMiddleware, videoUpload } from '../middleware';
+
+const videoRouter = express.Router();
+
+videoRouter.get('/:id([0-9a-f]{24})', watch);
+videoRouter
+  .route('/:id([0-9a-f]{24})/edit')
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(postEdit);
+videoRouter
+  .route('/:id([0-9a-f]{24})/delete')
+  .all(protectorMiddleware)
+  .get(deleteVideo);
+videoRouter
+  .route('/upload')
+  .all(protectorMiddleware)
+  .get(getUpload)
+  .post(
+    videoUpload.fields([
+      { name: 'video', maxCount: 1 },
+      { name: 'thumb', maxCount: 1 },
+    ]),
+    postUpload
+  );
+
+export default videoRouter;
